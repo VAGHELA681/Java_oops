638effc122f0f5c78e3d1572ec0bc49200142908
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-19 13:43:59 +0530

    One.java

---
+++ One.java
@@ -0,0 +1,44 @@
+//class = buleprint ✓
+class Student {
+
+   String name;  //variable
+
+   int age;
+
+ 
+//method
+   public void getInfo() {  
+
+       System.out.println("The name of this Student is "+ name);  // no error
+
+       System.out.println("The age of this Student is "+ age);
+
+   }
+
+}
+
+//class
+public class OOPS {
+//method
+   public static void main(String args[]) {
+//obj
+       Student s1 = new Student();
+//call
+       s1.name = "Aman";
+
+       s1.age = 24;
+
+       s1.getInfo();
+
+
+       Student s2 = new Student();
+
+       s2.name = "Shradha";
+
+       s2.age = 22;
+
+       s2.getInfo();
+
+   }
+
+}
